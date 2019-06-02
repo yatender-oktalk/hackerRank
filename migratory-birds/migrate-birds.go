b4169b2aa283fafb9f70d17c905bfd9ebd63c7cb
yatendernitk <y316nitk@gmail.com>
2019-06-02 19:01:07 +0530

    fixing stuff

--- migratory-birds/migrate-birds.go
+++ migratory-birds/migrate-birds.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"strings"
 	"bufio"
+	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -16,37 +16,36 @@ func main() {
 	text = strings.TrimSpace(text)
 	numList := strings.Split(text, " ")
 	m := make(map[int]int)
-	number:=0
+	number := 0
 	val := 0
 
-
-	for i:=range numList {
-		number,_ = strconv.Atoi(numList[i])
-		if isKeyExists(m,number ) {
+	for i := range numList {
+		number, _ = strconv.Atoi(numList[i])
+		if isKeyExists(m, number) {
 			//delete the key and increment the value
-			val = getKeyVal(m,number)
+			val = getKeyVal(m, number)
 			val++
-			updateKey(m,number,val)
+			updateKey(m, number, val)
 			//fmt.Println("exists")
-		}else{
+		} else {
 			// add the key in map
-			addKey(m,number)
+			addKey(m, number)
 			//fmt.Println("not exists")
 		}
 	}
 
-	number=0
+	number = 0
 	val = 0
 	maxFreq := 0
 	minNum := 200000
 	for key, value := range m {
-		if(value==maxFreq){
+		if value == maxFreq {
 			maxFreq = value
-			if(minNum>key){
+			if minNum > key {
 				minNum = key
 			}
 
-		}else if(value>maxFreq){
+		} else if value > maxFreq {
 			maxFreq = value
 			minNum = key
 		}
@@ -56,22 +55,21 @@ func main() {
 
 }
 
-
 func isKeyExists(m map[int]int, key int) bool {
 	//fmt.Println("isKeyExists ",key)
-	_,ok := m[key]
+	_, ok := m[key]
 	//fmt.Println(ok)
 	return ok
 }
 
-func addKey(m map[int]int, key int){
+func addKey(m map[int]int, key int) {
 	m[key] = 1
 }
 
-func updateKey(m map[int]int, key int, val int){
+func updateKey(m map[int]int, key int, val int) {
 	m[key] = val
 }
 
-func getKeyVal(m map[int]int, key int) int{
+func getKeyVal(m map[int]int, key int) int {
 	return m[key]
 }
